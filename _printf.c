3048e2f72e520098ac2fd575ce222f1183e4fcfc
Mboko David <98537263+Meekdavid@users.noreply.github.com>
2022-05-25 06:45:14 -0700

    push

--- _printf.c
+++ _printf.c
@@ -45,3 +45,176 @@ int _printf(const char *format, ...)
   return (cont);
   
 }
+/**
+
+ *loop_format - This is a lopping of the functions
+
+ *@arg: va_list arg
+
+ *@string: pointer from format
+
+ *Description: This function make loop
+
+ *Return: num of charactere
+
+ */
+
+int loop_format(va_list arg, const char *string)
+  
+{
+  
+  int i = 0, flag = 0, cont_fm = 0, cont = 0, check_per = 0;
+  
+
+  
+  while (i < _strlen((char *)string) && *string != '\0')
+    
+    {
+      
+      char aux = string[i];
+      
+
+      
+      if (aux == '%')
+	
+	{
+	  
+	  i++, flag++;
+	  
+	  aux = string[i];
+	  
+	  if (aux == '\0' && _strlen((char *)string) == 1)
+	    
+	    return (-1);
+	  
+	  if (aux == '\0')
+	    
+	    return (cont);
+	  
+	  if (aux == '%')
+	    
+	    {
+	      
+	      flag++;
+	      
+	    } else
+	      
+	    {
+	      
+	      cont_fm = function_manager(aux, arg);
+	      
+	      if (cont_fm >= 0 && cont_fm != -1)
+		
+		{
+		  
+		  i++;
+		  
+		  aux = string[i];
+		  
+		  if (aux == '%')
+		    
+		    flag--;
+		  
+		  cont = cont + cont_fm;
+		  
+		} else if (cont_fm == -1 && aux != '\n')
+		  
+		{
+		  
+		  cont += _putchar('%');
+		  
+		}
+	      
+	    }
+	  
+	}
+      
+      check_per = check_percent(&flag, aux);
+      
+      cont += check_per;
+      
+      if (check_per == 0 && aux != '\0' && aux != '%')
+	
+	cont += _putchar(aux), i++;
+      
+      check_per = 0;
+      
+    }
+  
+  return (cont);
+  
+}
+
+/**
+
+ * check_percent - call function
+
+ *@flag: value
+
+ *@aux: character
+
+ *Description: This function print
+
+ *Return: 1 if % is printed
+
+ */
+
+int check_percent(int *flag, char aux)
+  
+{
+  
+  int tmp_flag;
+  
+  int cont = 0;
+  
+
+  
+  tmp_flag = *flag;
+  
+  if (tmp_flag == 2 && aux == '%')
+    
+    {
+      
+      _putchar('%');
+      
+      tmp_flag = 0;
+      
+      cont = 1;
+      
+    }
+  
+  return (cont);
+  
+}
+
+
+
+/**
+
+ * call_function_manager
+
+ *@aux: character parameter
+
+ *@arg: va_list arg
+
+ *Description: This function call function
+
+ *Return: num of characteres
+
+ */
+
+
+
+int call_function_manager(char aux, va_list arg)
+  
+{
+  
+  int cont = 0;
+  
+
+  
+  cont = function_manager(aux, arg);
+  
+  return (cont);
+  
+}
